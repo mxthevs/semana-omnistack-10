85c65f27e048ff8dc2416548b90070a20e0dfea1
Matheus Henrique <mxthevsh@gmail.com>
2020-01-14 13:04:43 -0300

    Remove DeprecationWarning: collection.ensureIndex

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,7 @@ const app = express();
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-w4bif.mongodb.net/week10?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
 })
 
 app.use(express.json());
